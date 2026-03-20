9d58cfab6f306c7e8a99626eef5d3bd177d06aa2
Iman Datta <dattaiman56@gmail.com>
2026-03-21 00:46:13 +0530

    Connected TF-IDF vectorizer and Naive Bayes model to Django view for spam detection

--- mailguard_web/predictor/views.py
+++ mailguard_web/predictor/views.py
@@ -1,3 +1,23 @@
+import pickle
+import os
 from django.shortcuts import render
 
-# Create your views here.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+model_path = os.path.join(BASE_DIR, '..', 'notebook', 'model.pkl')
+vectorizer_path = os.path.join(BASE_DIR, '..', 'notebook', 'vectorizer.pkl')
+
+model = pickle.load(open(model_path, 'rb'))
+vectorizer = pickle.load(open(vectorizer_path, 'rb'))
+
+def home(request):
+    prediction = None
+    
+    if request.method == 'POST':
+        message = request.POST['message']
+        msg_vec = vectorizer.transform([message])
+        result = model.predict(msg_vec)[0]
+        
+        prediction = "Spam" if result == 1 else "Not Spam"
+
+    return render(request, 'index.html', {'prediction': prediction})
